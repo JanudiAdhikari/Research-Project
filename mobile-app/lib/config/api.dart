492d23445ef6c00bb6723c670cd0656ae099732c
Janudi Adhikari <156613694+JanudiAdhikari@users.noreply.github.com>
2025-12-03 22:36:31 +0530

    Update login and signup UIs

--- mobile-app/lib/config/api.dart
+++ mobile-app/lib/config/api.dart
@@ -3,5 +3,7 @@ class ApiConfig {
   // static const String baseUrl = "http://10.0.2.2:5000";
 
   // For physical device:
-  static const String baseUrl = "http://10.239.62.38:5000";
+  // cmd -> ipconfig -> IPv4 Address
+  // static const String baseUrl = "http://IPv4 Address:5000";
+  static const String baseUrl = "http://10.199.224.24:5000";
 }

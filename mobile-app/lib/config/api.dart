a81110f450cd840535e1fcca8fd48d34e154a7ad
induwaraR98 <icredu98@gmail.com>
2025-12-30 02:06:18 +0530

    create disease home screen and real weather data on dashboard

--- mobile-app/lib/config/api.dart
+++ mobile-app/lib/config/api.dart
@@ -1,9 +1,9 @@
 class ApiConfig {
   // emulator
- // static const String baseUrl = "http://10.0.2.2:5000";
+  // static const String baseUrl = "http://10.0.2.2:5000";
 
   // For physical device:
-  // cmd -> ipconfig -> IPv4 Address
+  // cmd -> ipconfig -> IPv4 Address (Wi-Fi adapter)
   // static const String baseUrl = "http://IPv4 Address:5000";
-   static const String baseUrl = "http://192.168.62.1:5000";
+  static const String baseUrl = "http://192.168.8.159:5000";
 }

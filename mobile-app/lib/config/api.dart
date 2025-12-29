6aaf420c7132981cbde24bc6b4293580efc1771f
induwaraR98 <icredu98@gmail.com>
2025-12-29 23:19:48 +0530

    initial commit by induwara

--- mobile-app/lib/config/api.dart
+++ mobile-app/lib/config/api.dart
@@ -1,9 +1,9 @@
 class ApiConfig {
   // emulator
-  static const String baseUrl = "http://10.0.2.2:5000";
+ // static const String baseUrl = "http://10.0.2.2:5000";
 
   // For physical device:
   // cmd -> ipconfig -> IPv4 Address
   // static const String baseUrl = "http://IPv4 Address:5000";
-  // static const String baseUrl = "http://10.199.224.24:5000";
+   static const String baseUrl = "http://192.168.62.1:5000";
 }

94264490a5efd60fdc92be514b4bd2014d326cca
Madara Meegama <madarameegama7@gmail.com>
2026-01-17 19:02:56 +0530

    New files with navaigation to bottom nav bar

--- mobile-app/lib/widgets/navigation_wrapper.dart
+++ mobile-app/lib/widgets/navigation_wrapper.dart
@@ -1,128 +1,46 @@
 import 'package:flutter/material.dart';
 import 'bottom_navigation.dart';
+import '../features/dashboard/farmer_dashboard.dart';
+import 'my_farm_screen.dart';
+import 'quality_screen.dart';
+import 'market_screen.dart';
+import 'profile_screen.dart';
 
 class NavigationWrapper extends StatefulWidget {
-  final Widget child;
-  final int initialIndex;
-  final bool showBottomNavigation;
-  final List<Widget> Function(int)? tabScreens;
-
-  const NavigationWrapper({
-    Key? key,
-    required this.child,
-    this.initialIndex = 0,
-    this.showBottomNavigation = true,
-    this.tabScreens,
-  }) : super(key: key);
+  const NavigationWrapper({Key? key}) : super(key: key);
 
   @override
   State<NavigationWrapper> createState() => _NavigationWrapperState();
 }
 
 class _NavigationWrapperState extends State<NavigationWrapper> {
-  late int _currentIndex;
+  int _currentIndex = 0;
 
-  @override
-  void initState() {
-    super.initState();
-    _currentIndex = widget.initialIndex;
-  }
+  final List<Widget> _pages = const [
+    FarmerDashboard(),
+    MyFarmScreen(),
+    QualityScreen(),
+    MarketScreen(),
+    ProfileScreen(),
+  ];
 
   void _onTabSelected(int index) {
     setState(() {
       _currentIndex = index;
     });
-
-    // If tabScreens are provided, handle navigation differently
-    if (widget.tabScreens != null) {
-      // You might want to use Navigator to push screens
-      // or manage with a PageController for tab navigation
-    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: widget.child,
-      bottomNavigationBar: widget.showBottomNavigation
-          ? BottomNavigation(
-              currentIndex: _currentIndex,
-              onTabSelected: _onTabSelected,
-            )
-          : null,
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigation(
+        currentIndex: _currentIndex,
+        onTabSelected: _onTabSelected,
+      ),
     );
   }
 }
-
-//v2
-// import 'package:flutter/material.dart';
-// import 'bottom_navigation.dart';
-//
-// class NavigationWrapper extends StatefulWidget {
-//   final List<Widget> screens;
-//   final int initialIndex;
-//   final bool showBottomNavigation;
-//
-//   const NavigationWrapper({
-//     Key? key,
-//     required this.screens,
-//     this.initialIndex = 0,
-//     this.showBottomNavigation = true,
-//   }) : super(key: key);
-//
-//   @override
-//   State<NavigationWrapper> createState() => _NavigationWrapperState();
-// }
-//
-// class _NavigationWrapperState extends State<NavigationWrapper> {
-//   late int _currentIndex;
-//   late PageController _pageController;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _currentIndex = widget.initialIndex;
-//     _pageController = PageController(initialPage: widget.initialIndex);
-//   }
-//
-//   @override
-//   void dispose() {
-//     _pageController.dispose();
-//     super.dispose();
-//   }
-//
-//   void _onTabSelected(int index) {
-//     setState(() {
-//       _currentIndex = index;
-//     });
-//     _pageController.animateToPage(
-//       index,
-//       duration: const Duration(milliseconds: 300),
-//       curve: Curves.easeInOut,
-//     );
-//   }
-//
-//   void _onPageChanged(int index) {
-//     setState(() {
-//       _currentIndex = index;
-//     });
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: PageView(
-//         controller: _pageController,
-//         onPageChanged: _onPageChanged,
-//         physics: const NeverScrollableScrollPhysics(), // Disable swipe navigation
-//         children: widget.screens,
-//       ),
-//       bottomNavigationBar: widget.showBottomNavigation
-//           ? BottomNavigation(
-//         currentIndex: _currentIndex,
-//         onTabSelected: _onTabSelected,
-//       )
-//           : null,
-//     );
-//   }
-// }

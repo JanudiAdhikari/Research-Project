e6686174b1a9c19e2241fd99ef3bdddf23d41f5a
Janudi Adhikari <156613694+JanudiAdhikari@users.noreply.github.com>
2026-02-25 01:52:46 +0530

    Implement backend for Image Upload part

--- mobile-app/lib/config/api.dart
+++ mobile-app/lib/config/api.dart
@@ -5,6 +5,6 @@ class ApiConfig {
   // For physical device:
   // cmd -> ipconfig -> IPv4 Address (Wi-Fi adapter)
   // static const String baseUrl = "http://IPv4 Address:5000";
-  //  static const String baseUrl = "http://10.245.15.36:5000";
-   static const String fastApiBaseUrl = "http://10.245.15.36:8000"; // inference server
+  //  static const String baseUrl = "http://10.126.124.24:5000";
+   static const String fastApiBaseUrl = "http://10.126.124.24:8000"; // inference server
 }

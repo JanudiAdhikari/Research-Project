95b12ce438b5548d575de299a0eade6fe33f7809
Janudi Adhikari <156613694+JanudiAdhikari@users.noreply.github.com>
2026-02-28 00:24:54 +0530

    Update past quality reports folder structure

--- mobile-app/lib/features/quality_grading/screens/past_reports/detailed_report_screen.dart
+++ mobile-app/lib/features/quality_grading/screens/past_reports/detailed_report_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../../utils/responsive.dart';
+import '../../../../utils/responsive.dart';
 
 class DetailedReportScreen extends StatefulWidget {
   const DetailedReportScreen({super.key});
